229b5f37d75d34bec3e6e362c14c2154d238241d
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-01-24 21:38:26 -0500

    added code for parsing through users input using inquirer package

--- Develop/index.js
+++ Develop/index.js
@@ -4,74 +4,115 @@ console.log("you are running my generator!");
 const questions = [
   {
     type: "input",
-    name: "title",
+    name: "Title",
     message: "What is the title of your project?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter a valid Title!");
+    },
   },
   {
     type: "input",
-    name: "description",
-    message: "Using a few sentences, please describe your project.",
-    validat() {},
+    name: "Description",
+    message:
+      "Using a few sentences, please describe your project.(180 character minimum)",
+    validate(value) {
+      if (value.length > 180) return true;
+      else console.log("You must enter a longer Description!");
+    },
   },
   {
     type: "input",
-    name: "installation",
+    name: "Installation",
     message: "Where can the code for this project be found?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter a valid URL!");
+    },
   },
   {
     type: "input",
-    name: "usage",
+    name: "Usage",
     message: "What might this project be used for?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter a valid response!");
+    },
   },
   {
     type: "input",
-    name: "contributing",
+    name: "Contributing",
     message: "Who contributed to this project?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter contributers!");
+    },
   },
   {
     type: "input",
-    name: "tests",
+    name: "Tests",
     message: "Please lists any tests you have for this project.",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter a valid test option!");
+    },
   },
   {
     type: "input",
-    name: "features",
+    name: "Features",
     message: "What are some notable features of your project?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter a valid features!");
+    },
   },
   {
     type: "input",
-    name: "badges",
+    name: "Badges",
     message: "Do you have any badges?",
-    validat() {},
+    validate(value) {
+      if (value !== "") return true;
+      else console.log("You must enter valid badges!");
+    },
   },
   {
     type: "list",
     name: "License",
     message: "What type of license will you use?",
-    choices: "MIT",
+    choices: [
+      "MIT",
+      "Apache",
+      "Boost",
+      "Eclipse",
+      "The Hippocratic",
+      "IBM",
+      "ISC",
+      "Mozilla",
+    ],
   },
 ];
 // Title, Description, Installation, Usage, Contributing, and Tests
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
+// Created a function to initialize app
+function init(inquirer) {}
 const inquirer = require("inquirer");
 inquirer
   .prompt(questions)
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    console.log(answers);
+    const toc = [];
+    for (let i = 0; i < questions.length; i++) {
+      console.log(questions[i].name);
+      toc.push(questions[i].name);
+      // Use user feedback for... whatever!!
+    }
+    console.log(toc);
   })
   .catch((error) => {
     if (error.isTtyError) {
+      console.log("Something went wrong!");
       // Prompt couldn't be rendered in the current environment
     } else {
       // Something else went wrong
--- README.md
+++ README.md
@@ -2,15 +2,50 @@
 
 #Description
 
+This project was given during the 9th week of my bootcamp. The goal of this project is to create a project using Node.js and the inquirer package in order to generate a readme file using the users input.
+
 #Installation
+
+Code for this project can be found at : https://github.com/gardnerd06/dynamic-readme-generator
 
 #Usage
 
+The purpose of this project is to dynamically create a Readme file from the users input.
+
 #Contributing
+
+Credit for this project is given to:
+
+Node.js - https://nodejs.org/en/
+Inquirer Package - https://www.npmjs.com/package/inquirer
 
 #Screenshots
 
 #License
+
+The MIT License was used for this project:
+
+MIT License
+
+Copyright (c) 2023 Daniel Gardner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 <!-- This is a Dynamic Proffesional Readme file generator using the inquirer package
 
